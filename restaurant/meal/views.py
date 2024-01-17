3a5e094ea35e79feb24417ce33ffd69bfbb9d9c5
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-17 15:02:16 +0600

    add apps

--- restaurant/meal/views.py
+++ restaurant/meal/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def meal(request):
+    return render(request, 'meal/meal.html')
--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.home),
+    path('meal/', include('meal.urls')),
+    path('about_us/', include('about_us.urls'))
 ]
