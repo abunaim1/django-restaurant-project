a87764e0ebc4f6e5f7d6aabfa9fc097557aedd35
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-17 16:49:37 +0600

    home done

--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
     path('meal/', include('meal.urls')),
     path('about_us/', include('about_us.urls'))
 ]
